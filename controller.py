8b3550d758478c3d7f6637efc6059c594b8d46fe
Jeff <--global>
2021-05-15 16:21:34 -0400

    adding plot of trajectory

--- controller.py
+++ controller.py
@@ -14,6 +14,7 @@ except IndexError:
 
 import carla
 import random
+import copy
 import time
 from carEnv import *
 
@@ -21,22 +22,37 @@ class Controller:
     # goal
     def __init__(self,vehicle):
         self.vehicle = vehicle
-        self.X = np.zeros(3)
+        self.data_len = 1000000
+        self.data_tick = 0
+        self.X_hist = np.zeros((3,self.data_len))
         self.dX = np.zeros(3)
-        # self.goal = np.zeros(2)
+        self.X_init = np.zeros(3)
         self.arrived_goal = False
+        self.init_state()
+
+    def init_state(self):
+        T = self.vehicle.get_transform()
+        self.X_init = np.array([T.location.x,T.location.y,T.rotation.yaw])
 
     def apply(self,throttle,steer):
         self.vehicle.apply_control(carla.VehicleControl(throttle, steer))
 
     def update_state(self):
         T = self.vehicle.get_transform()
-        self.X[0] = T.location.x
-        self.X[1] = T.location.y
-        self.X[2] = T.rotation.yaw
-        self.arrived_goal = np.linalg.norm(self.X[:2] - self.goal) < 2.0
+        self.X_hist[0,self.data_tick] = T.location.x
+        self.X_hist[1,self.data_tick] = T.location.y
+        self.X_hist[2,self.data_tick] = T.rotation.yaw
+        if self.data_tick + 1 == self.data_len:
+            new_hist = np.zeros((3,self.data_len*2))
+            new_hist[:,0:self.data_len] = copy.deepcopy(self.X_hist)
+            self.data_len *= 2
+            self.X_hist = copy.deepcopy(new_hist)
+        self.arrived_goal = np.linalg.norm(self.X_hist[:2,self.data_tick] - self.goal) < 2.0
         # p = np.linalg.norm(self.X[:2] - self.goal)
-        # print(p,self.X[:2])
+
+    def get_traj(self):
+        ## return init-zeroed trajectory
+        return (self.X_hist[:,:self.data_tick].T-self.X_init[:,np.newaxis].T).T
 
 class V1Controller(Controller):
     def __init__(self,vehicle):
@@ -52,8 +68,8 @@ class CurveFollowController(Controller):
         super().__init__(vehicle)
 
     def calculate_steer(self):
-        r = np.linalg.norm(self.o-self.X[:2])
-        steer = (self.r_d-r)/(self.r_d*0.4)
+        r = np.linalg.norm(self.o-self.X_hist[:2,self.data_tick])
+        steer = (self.r_d-r)/(self.r_d*0.25)
         return steer
 
     def calculate_throttle(self):
@@ -62,3 +78,4 @@ class CurveFollowController(Controller):
     def step(self):
         super().update_state()
         super().apply(self.calculate_throttle(),self.calculate_steer())
+        self.data_tick += 1
--- project_main.py
+++ project_main.py
@@ -2,6 +2,9 @@ import glob
 import os
 import sys
 from sys import exit
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -17,15 +20,25 @@ import time
 from carEnv import *
 
 def main():
-    env = carEnv();
+    env = carEnv()
     # os.system('python manual_control_custom.py -z 1')
+    time.sleep(2);
+    env.init_Controller()
     try:
         while True:
             while not env.c2.arrived_goal:
                 env.c2.step()
             for actor in env.actor_list:
                 actor.destroy()
-            env.reset()
+            break;
+            # env.reset()
+        traj = env.c2.get_traj()
+        P, (ax1, ax2, ax3) = plt.subplots(3, 1)
+        ax1.plot(traj[0,:].T,'r');ax1.set_title('x')
+        ax2.plot(traj[1,:].T,'g');ax2.set_title('y')
+        ax3.plot(traj[2,:].T,'b');ax3.set_title(r"$\theta$")
+        plt.ticklabel_format(useOffset=False)
+        plt.show()
     except KeyboardInterrupt:
         for actor in env.actor_list:
             actor.destroy()
